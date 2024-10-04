dbbe238e5cc479ab388525e0a4205ac051b52eec
rien88 <112721833+rien88@users.noreply.github.com>
2024-10-04 19:46:35 -0400

    Feature/humble (#4)
    * Fixed cmakelists and package for ros2
    
    * Builds now
    
    * Bugfix so now can run the launch
    
    * Got it running and added clustering, needs further tuning for trail finding
    
    * Removed max cluster size, and made clustering run only on ground cloud
    
    * Added count window for clustering and ground segment
    
    * Added manually setting pointcloud frame id bc on some runs its not assigned
    
    * Added cloud reset after every clustering round
    
    * Removed commented out code
    
    * Tested variations on identifying the best cluster, currently set to choose the largest, also explore add tracking adjacent detections over time
    
    * Added line segment extraction and marker publishing from trail
    
    * adding percent-above estimator
    
    * Added do_trail arg to reduce compute requirements when not needed
    
    * Switched cout to rclcpp_info
    
    ---------
    
    Co-authored-by: Gus Meyer <gus@robotics88.com>

--- include/pcl_analysis/pcl_analysis.h
+++ include/pcl_analysis/pcl_analysis.h
@@ -6,39 +6,53 @@ Author: Gus Meyer <gus@robotics88.com>
 #ifndef PCL_ANALYSIS_H_
 #define PCL_ANALYSIS_H_
 
-#include <ros/ros.h>
+#include <rclcpp/rclcpp.hpp>
 
-#include <sensor_msgs/PointCloud2.h>
-#include <pcl_ros/point_cloud.h>
+#include "geometry_msgs/msg/pose_stamped.hpp"
+#include "std_msgs/msg/float32.hpp"
+#include "sensor_msgs/msg/point_cloud2.hpp"
+#include <pcl/point_cloud.h>
+#include "pcl/point_types.h"
+#include "pcl_conversions/pcl_conversions.h"
+
+#include "visualization_msgs/msg/marker.hpp"
 
 /**
  * @class PCLAnalysis
  * @brief A class for analyzing and segmenting point clouds
  */
-class PCLAnalysis {
+class PCLAnalysis : public rclcpp::Node {
 
     public:
-        PCLAnalysis(ros::NodeHandle& node);
+        PCLAnalysis();
         ~PCLAnalysis();
 
-        void pointCloudCallback(const sensor_msgs::PointCloud2::ConstPtr &msg);
+        void localPositionCallback(const geometry_msgs::msg::PoseStamped::SharedPtr msg);
+        void pointCloudCallback(const sensor_msgs::msg::PointCloud2::SharedPtr msg);
 
-        void timerCallback(const ros::TimerEvent&);
+        void timerCallback();
 
     private: 
-        ros::NodeHandle nh_;
-        ros::NodeHandle private_nh_;
-
+        bool pcl_time_;
+        int count_;
         std::string     point_cloud_topic_;
-        ros::Subscriber point_cloud_subscriber_;
+        rclcpp::Subscription<geometry_msgs::msg::PoseStamped>::SharedPtr    mavros_local_pos_subscriber_;
+        rclcpp::Subscription<sensor_msgs::msg::PointCloud2>::SharedPtr      point_cloud_subscriber_;
 
-        ros::Publisher cloud_ground_pub_;
-        ros::Publisher cloud_nonground_pub_;
+        rclcpp::Publisher<sensor_msgs::msg::PointCloud2>::SharedPtr         cloud_ground_pub_;
+        rclcpp::Publisher<sensor_msgs::msg::PointCloud2>::SharedPtr         cloud_nonground_pub_;
+        rclcpp::Publisher<sensor_msgs::msg::PointCloud2>::SharedPtr         cloud_cluster_pub_;
+        rclcpp::Publisher<std_msgs::msg::Float32>::SharedPtr                percent_above_pub_;
+        rclcpp::Publisher<visualization_msgs::msg::Marker>::SharedPtr       trail_line_pub_;
 
-        ros::Timer timer_;
+        rclcpp::Time last_pub_time_;
 
         // Main input pointcloud holder
         pcl::PointCloud<pcl::PointXYZ>::Ptr cloud_latest_{new pcl::PointCloud<pcl::PointXYZ>()};
+
+        // Trail line
+        visualization_msgs::msg::Marker trail_marker_;
+        bool do_trail_;
 
         // Params
         double  pub_rate_;
@@ -48,6 +62,14 @@ class PCLAnalysis {
         float   pmf_slope_;
         float   pmf_initial_distance_;
         float   pmf_max_distance_;
+
+        geometry_msgs::msg::PoseStamped current_pose_;
+
+        void findTrail(const pcl::PointCloud<pcl::PointXYZ>::Ptr cloud,
+                             pcl::PointCloud<pcl::PointXYZ>::Ptr cloud_clustered);
+        pcl::PointCloud<pcl::PointXYZ>::Ptr findMaximumPlanar(const pcl::PointCloud<pcl::PointXYZ>::Ptr cloud_clustered,
+                                                         const std::vector<pcl::PointIndices> cluster_indices);
+        void extractLineSegment(const pcl::PointCloud<pcl::PointXYZ>::Ptr cloud_clustered);
 
         // Segments out a plane from a pointcloud including points within segment_distance_threshold_ of plane model
         void segment_plane(const pcl::PointCloud<pcl::PointXYZ>::Ptr cloud,
@@ -65,6 +87,9 @@ class PCLAnalysis {
 
         // Runs a voxel grid filter to downsample pointcloud into 3D grid of leaf_size
         void voxel_grid_filter(pcl::PointCloud<pcl::PointXYZ>::Ptr cloud, float leaf_size);
+
+        // Determines percentage of pointcloud points above drone
+        float get_percent_above(pcl::PointCloud<pcl::PointXYZ>::Ptr cloud);
 
 }; // class PCLAnalysis
 
--- src/pcl_analysis.cpp
+++ src/pcl_analysis.cpp
@@ -7,15 +7,22 @@ Author: Gus Meyer <gus@robotics88.com>
 #include <pcl/ModelCoefficients.h>
 #include <pcl/sample_consensus/method_types.h>
 #include <pcl/sample_consensus/model_types.h>
+#include <pcl/sample_consensus/sac_model_stick.h>
 #include <pcl/segmentation/sac_segmentation.h>
 #include <pcl/filters/extract_indices.h>
 #include <pcl/filters/voxel_grid.h>
+#include <pcl/common/io.h>
 
 #include <pcl/segmentation/progressive_morphological_filter.h>
+#include <pcl/segmentation/extract_clusters.h>
 
-PCLAnalysis::PCLAnalysis(ros::NodeHandle& node)
-    : private_nh_("~")
-    , nh_(node)
+using std::placeholders::_1;
+
+PCLAnalysis::PCLAnalysis()
+    : Node("pcl_analysis")
+    , pcl_time_(false)
+    , count_(0)
+    , do_trail_(false)
     , pub_rate_(2.0)
     , point_cloud_topic_("")
     , segment_distance_threshold_(0.01)
@@ -24,58 +31,239 @@ PCLAnalysis::PCLAnalysis(ros::NodeHandle& node)
     , pmf_slope_(1.0)
     , pmf_initial_distance_(0.5)
     , pmf_max_distance_(3.0)
+    , last_pub_time_(0, 0, RCL_ROS_TIME)
 {
     // Get params
-    private_nh_.param<double>("pub_rate", pub_rate_, pub_rate_);
-    private_nh_.param<std::string>("point_cloud_topic", point_cloud_topic_, point_cloud_topic_);
-    private_nh_.param<double>("segment_distance_threshold", segment_distance_threshold_, segment_distance_threshold_);
-    private_nh_.param<float>("voxel_grid_leaf_size", voxel_grid_leaf_size_, voxel_grid_leaf_size_);
+    this->declare_parameter("do_trail", do_trail_);
+    this->declare_parameter("pub_rate", pub_rate_);
+    this->declare_parameter("point_cloud_topic", point_cloud_topic_);
+    this->declare_parameter("segment_distance_threshold", segment_distance_threshold_);
+    this->declare_parameter("voxel_grid_leaf_size", voxel_grid_leaf_size_);
+    this->declare_parameter("pmf_max_window_size", pmf_max_window_size_);
+    this->declare_parameter("pmf_slope", pmf_slope_);
+    this->declare_parameter("pmf_initial_distance", pmf_initial_distance_);
+    this->declare_parameter("pmf_max_distance", pmf_max_distance_);
 
-    private_nh_.param<int>("pmf_max_window_size", pmf_max_window_size_, pmf_max_window_size_);
-    private_nh_.param<float>("pmf_slope", pmf_slope_, pmf_slope_);
-    private_nh_.param<float>("pmf_initial_distance", pmf_initial_distance_, pmf_initial_distance_);
-    private_nh_.param<float>("pmf_max_distance", pmf_max_distance_, pmf_max_distance_);
+    this->get_parameter("do_trail", do_trail_);
+    this->get_parameter("pub_rate", pub_rate_);
+    this->get_parameter("point_cloud_topic", point_cloud_topic_);
+    this->get_parameter("segment_distance_threshold", segment_distance_threshold_);
+    this->get_parameter("voxel_grid_leaf_size", voxel_grid_leaf_size_);
+    this->get_parameter("pmf_max_window_size", pmf_max_window_size_);
+    this->get_parameter("pmf_slope", pmf_slope_);
+    this->get_parameter("pmf_initial_distance", pmf_initial_distance_);
+    this->get_parameter("pmf_max_distance", pmf_max_distance_);
 
     // Set up pubs and subs
-    point_cloud_subscriber_ = nh_.subscribe<sensor_msgs::PointCloud2>(point_cloud_topic_, 10, &PCLAnalysis::pointCloudCallback, this);
+    mavros_local_pos_subscriber_ = this->create_subscription<geometry_msgs::msg::PoseStamped>("/mavros/local_position/pose", rclcpp::SensorDataQoS(), std::bind(&PCLAnalysis::localPositionCallback, this, _1));
 
-    cloud_ground_pub_ = nh_.advertise<sensor_msgs::PointCloud2>("cloud_ground", 10);
-    cloud_nonground_pub_ = nh_.advertise<sensor_msgs::PointCloud2>("cloud_nonground", 10);
+    point_cloud_subscriber_ = this->create_subscription<sensor_msgs::msg::PointCloud2>(point_cloud_topic_, 10, std::bind(&PCLAnalysis::pointCloudCallback, this, _1));
 
-    // Set up timer for pointcloud processing and publication
-    timer_ = nh_.createTimer(ros::Duration(1.0/pub_rate_), &PCLAnalysis::timerCallback, this);
+    cloud_ground_pub_ = this->create_publisher<sensor_msgs::msg::PointCloud2>("cloud_ground", 10);
+    cloud_nonground_pub_ = this->create_publisher<sensor_msgs::msg::PointCloud2>("cloud_nonground", 10);
+    cloud_cluster_pub_ = this->create_publisher<sensor_msgs::msg::PointCloud2>("cloud_clusters", 10);
 
+    percent_above_pub_ = this->create_publisher<std_msgs::msg::Float32>("~/percent_above", 10);
+
+    trail_line_pub_ = this->create_publisher<visualization_msgs::msg::Marker>("/trail_line", 10);
+    trail_marker_.header.frame_id = "map";
+    trail_marker_.scale.x = 3.0;
+    trail_marker_.type = visualization_msgs::msg::Marker::LINE_STRIP;
+    trail_marker_.action = visualization_msgs::msg::Marker::ADD;
+    trail_marker_.id = 0;
 }
 
 PCLAnalysis::~PCLAnalysis(){}
 
-void PCLAnalysis::timerCallback(const ros::TimerEvent&) {
-
-    // Make sure we have recent pointcloud to work with
-    ros::Time last_cloud_stamp;
-    pcl_conversions::fromPCL(cloud_latest_->header.stamp, last_cloud_stamp);
-    double seconds_since_last_cloud = (ros::Time::now() - last_cloud_stamp).toSec();
-    if (seconds_since_last_cloud > 1.0/pub_rate_) {return;}
-    
-    // Downsample cloud for processing
-    voxel_grid_filter(cloud_latest_, voxel_grid_leaf_size_);
-
-    // Extract ground returns
-    pcl::PointCloud<pcl::PointXYZ>::Ptr cloud_ground(new pcl::PointCloud<pcl::PointXYZ>());
-    pcl::PointCloud<pcl::PointXYZ>::Ptr cloud_nonground(new pcl::PointCloud<pcl::PointXYZ>());
-    pmf_ground_extraction(cloud_latest_, cloud_ground, cloud_nonground);
-
-    // Convert to ROS msg and publish
-    sensor_msgs::PointCloud2 cloud_ground_msg, cloud_nonground_msg;
-    pcl::toROSMsg(*cloud_ground, cloud_ground_msg);
-    pcl::toROSMsg(*cloud_nonground, cloud_nonground_msg);
-    cloud_ground_pub_.publish(cloud_ground_msg);
-    cloud_nonground_pub_.publish(cloud_nonground_msg);
+void PCLAnalysis::localPositionCallback(const geometry_msgs::msg::PoseStamped::SharedPtr msg) {
+    current_pose_ = *msg;
 }
 
-void PCLAnalysis::pointCloudCallback(const sensor_msgs::PointCloud2::ConstPtr &msg) {
+void PCLAnalysis::pointCloudCallback(const sensor_msgs::msg::PointCloud2::SharedPtr msg) {
+
+    // Only run processing at limited rate
+    rclcpp::Duration dur = this->get_clock()->now() - last_pub_time_;
+    if (dur.seconds() < (1.0 / pub_rate_)) {
+        return;
+    }
+
     // Convert ROS msg to PCL and store
-    pcl::fromROSMsg(*msg, *cloud_latest_);
+    pcl::PointCloud<pcl::PointXYZ>::Ptr cloud(new pcl::PointCloud<pcl::PointXYZ>());
+    pcl::fromROSMsg(*msg, *cloud);
+    
+    // Downsample cloud for processing
+    voxel_grid_filter(cloud, voxel_grid_leaf_size_);
+
+    // Determine percentage of points above current location
+    float percent = get_percent_above(cloud);
+    std_msgs::msg::Float32 percent_above_msg;
+    percent_above_msg.data = percent;
+    percent_above_pub_->publish(percent_above_msg);
+
+    if (do_trail_) {
+        // Extract ground returns
+        pcl::PointCloud<pcl::PointXYZ>::Ptr cloud_ground(new pcl::PointCloud<pcl::PointXYZ>());
+        pcl::PointCloud<pcl::PointXYZ>::Ptr cloud_nonground(new pcl::PointCloud<pcl::PointXYZ>());
+        pmf_ground_extraction(cloud, cloud_ground, cloud_nonground);
+
+        // Cluster ground returns to find the trail
+        pcl::PointCloud<pcl::PointXYZ>::Ptr cloud_clustered(new pcl::PointCloud<pcl::PointXYZ>());
+        findTrail(cloud_ground, cloud_clustered);
+
+        // Convert to ROS msg and publish
+        sensor_msgs::msg::PointCloud2 cloud_ground_msg, cloud_nonground_msg, cloud_clustered_msg;
+        pcl::toROSMsg(*cloud_ground, cloud_ground_msg);
+        pcl::toROSMsg(*cloud_nonground, cloud_nonground_msg);
+        pcl::toROSMsg(*cloud_clustered, cloud_clustered_msg);
+        cloud_ground_pub_->publish(cloud_ground_msg);
+        cloud_nonground_pub_->publish(cloud_nonground_msg);
+        cloud_clustered_msg.header = cloud_ground_msg.header;
+        cloud_clustered_msg.header.frame_id = "map"; // TODO why is it sometimes missing the frame?
+        cloud_cluster_pub_->publish(cloud_clustered_msg);
+
+        trail_line_pub_->publish(trail_marker_);
+    }
+
+    pcl_time_ = false;
+
+    last_pub_time_ = this->get_clock()->now();
+}
+
+void PCLAnalysis::findTrail(const pcl::PointCloud<pcl::PointXYZ>::Ptr cloud,
+                                      pcl::PointCloud<pcl::PointXYZ>::Ptr cloud_clustered) {
+    // Set up the clustering
+    pcl::search::KdTree<pcl::PointXYZ>::Ptr tree(new pcl::search::KdTree<pcl::PointXYZ>());
+    tree->setInputCloud(cloud);
+
+    std::vector<pcl::PointIndices> cluster_indices;
+    pcl::EuclideanClusterExtraction<pcl::PointXYZ> ec;
+    ec.setClusterTolerance(1.0); // Adjust tolerance as necessary
+    ec.setMinClusterSize(50); // Minimum number of points to form a cluster
+    // ec.setMaxClusterSize(25000); // Maximum number of points to form a cluster
+    ec.setSearchMethod(tree);
+    ec.setInputCloud(cloud);
+    ec.extract(cluster_indices);
+    if (cluster_indices.size() == 0) return;
+    // TEST: Get largest cluster
+    int max_index = 0, max_size = 0;
+    for (int ii = 0; ii < cluster_indices.size(); ii++) {
+        if (cluster_indices.at(ii).indices.size() > max_size) {
+            max_index = ii;
+            max_size = cluster_indices.at(ii).indices.size();
+        }
+    }
+    
+    // pcl::PointCloud<pcl::PointXYZ>::Ptr cluster_points(new pcl::PointCloud<pcl::PointXYZ>);
+    pcl::PointIndices::Ptr cluster(new pcl::PointIndices());
+    *cluster = cluster_indices.at(max_index);
+    pcl::ExtractIndices<pcl::PointXYZ> extract;
+    extract.setInputCloud(cloud);
+    extract.setIndices(cluster);
+    extract.filter(*cloud_clustered);
+
+    // TEST: Get cluster best fit to shape
+    // cloud_clustered = findMaximumPlanar(cloud, cluster_indices);
+
+    // // Create a colored point cloud for visualization
+    // cloud_clustered->resize(cloud->points.size());
+    
+    // Original, just return all clusters
+    // int j = 0;
+    // for (const auto& indices : cluster_indices) {
+    //     uint32_t color = rand() % 0xFFFFFF; // Random color
+    //     for (const auto& index : indices.indices) {
+    //         cloud_clustered->points[index].x = cloud->points[index].x;
+    //         cloud_clustered->points[index].y = cloud->points[index].y;
+    //         cloud_clustered->points[index].z = cloud->points[index].z;
+    //         cloud_clustered->points[index].r = (color >> 16) & 0xFF;
+    //         cloud_clustered->points[index].g = (color >> 8) & 0xFF;
+    //         cloud_clustered->points[index].b = color & 0xFF;
+    //     }
+    //     j++;
+    // }
+
+    // Extract line segment and append to trail marker list
+    extractLineSegment(cloud_clustered);
+}
+
+void PCLAnalysis::extractLineSegment(const pcl::PointCloud<pcl::PointXYZ>::Ptr cloud_clustered) {
+    // Perform RANSAC plane fitting
+    pcl::ModelCoefficients::Ptr coefficients (new pcl::ModelCoefficients);
+    pcl::PointIndices::Ptr inliers (new pcl::PointIndices);
+    pcl::SACSegmentation<pcl::PointXYZ> seg;
+    seg.setOptimizeCoefficients(true);
+    seg.setModelType(pcl::SACMODEL_LINE);
+    seg.setMethodType(pcl::SAC_RANSAC);
+    seg.setDistanceThreshold(0.1); // Set distance threshold for inliers
+    seg.setInputCloud(cloud_clustered);
+    seg.segment(*inliers, *coefficients);
+
+    if (inliers->indices.size() == 0) {
+        RCLCPP_INFO(this->get_logger(), "Could not estimate a linear model for the given dataset.");
+    }
+
+    // Extract the line segment endpoints from the coefficients
+    geometry_msgs::msg::Point point;
+    point.x = coefficients->values[0];
+    point.y = coefficients->values[1];
+    point.z = coefficients->values[2];
+    trail_marker_.points.push_back(point);
+
+    // Second set of coefficients is a direction vector
+    point.x += coefficients->values[3];
+    point.y += coefficients->values[4];
+    point.z += coefficients->values[5];
+    trail_marker_.points.push_back(point);
+}
+
+pcl::PointCloud<pcl::PointXYZ>::Ptr PCLAnalysis::findMaximumPlanar(const pcl::PointCloud<pcl::PointXYZ>::Ptr cloud_clustered,
+                                    const std::vector<pcl::PointIndices> cluster_indices) {
+    // Iterate through each cluster
+    int index = 0, count = 0;
+    float max_ratio = 0.0;
+    for (int ii = 0; ii < cluster_indices.size(); ii++) {
+    // for (pcl::PointIndices cluster : cluster_indices) {
+        if (cluster_indices.at(ii).indices.size() < 1000) {
+            continue;
+        }
+        pcl::PointIndices::Ptr cluster(new pcl::PointIndices());
+        *cluster = cluster_indices.at(ii);
+        pcl::PointCloud<pcl::PointXYZ> cluster_points;
+        pcl::ExtractIndices<pcl::PointXYZ> extract;
+        extract.setInputCloud(cloud_clustered);
+        extract.setIndices(cluster);
+        extract.filter(cluster_points);
+
+        // Perform RANSAC plane fitting
+        pcl::ModelCoefficients::Ptr coefficients (new pcl::ModelCoefficients);
+        pcl::PointIndices::Ptr inliers (new pcl::PointIndices);
+        pcl::SACSegmentation<pcl::PointXYZ> seg;
+        seg.setOptimizeCoefficients(true);
+        seg.setModelType(pcl::SACMODEL_LINE);
+        seg.setMethodType(pcl::SAC_RANSAC);
+        seg.setDistanceThreshold(0.1); // Set distance threshold for inliers
+        seg.setInputCloud(cluster_points.makeShared());
+        seg.segment(*inliers, *coefficients);
+
+        // Calculate planarity metric (e.g., inlier ratio)
+        float inlier_ratio = static_cast<float>(inliers->indices.size()) / cluster_points.size();
+
+        // Store the planarity metric for comparison
+        if (inlier_ratio > max_ratio) {
+            index = count;
+        }
+        count++;
+    }
+
+    pcl::PointCloud<pcl::PointXYZ>::Ptr cluster_points(new pcl::PointCloud<pcl::PointXYZ>);
+    pcl::PointIndices::Ptr cluster(new pcl::PointIndices());
+    *cluster = cluster_indices.at(index);
+    pcl::ExtractIndices<pcl::PointXYZ> extract;
+    extract.setInputCloud(cloud_clustered);
+    extract.setIndices(cluster);
+    extract.filter(*cluster_points);
+    
+    return cluster_points;
 }
 
 void PCLAnalysis::segment_plane(const pcl::PointCloud<pcl::PointXYZ>::Ptr cloud,
@@ -97,7 +285,7 @@ void PCLAnalysis::segment_plane(const pcl::PointCloud<pcl::PointXYZ>::Ptr cloud,
 
     if (inliers->indices.size () == 0)
     {
-        ROS_DEBUG("Could not estimate a planar model from pointcloud.");
+        RCLCPP_INFO(this->get_logger(),"Could not estimate a planar model from pointcloud.");
         return;
     }
 
@@ -155,4 +343,20 @@ void PCLAnalysis::voxel_grid_filter(pcl::PointCloud<pcl::PointXYZ>::Ptr cloud, f
     sor.setInputCloud (cloud);
     sor.setLeafSize (leaf_size, leaf_size, leaf_size);
     sor.filter (*cloud);
+}
+
+float PCLAnalysis::get_percent_above(pcl::PointCloud<pcl::PointXYZ>::Ptr cloud) {
+    if (cloud->points.size() < 1) {
+        RCLCPP_ERROR(this->get_logger(), "Attempting to process invalid PCL");
+        return -1.0;
+    }
+
+    int num_pts_above = 0;
+    for (const auto & point : cloud->points) {
+        if (point.z > current_pose_.pose.position.z) {
+            num_pts_above++;
+        }
+    }
+
+    return static_cast<float>(num_pts_above) / cloud->points.size();
 }
